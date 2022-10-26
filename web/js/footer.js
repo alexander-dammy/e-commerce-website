0cf8523a129e88a6543818bcfe4bdabf2ad5291d
alexander-dammy <115098550+alexander-dammy@users.noreply.github.com>
2022-10-26 11:56:29 +0100

    Add files via upload

---
+++ web/js/footer.js
@@ -0,0 +1,61 @@
+const createFooter = () => {
+    let footer = document.querySelector('footer');
+
+    footer.innerHTML =  '<div class="footer-content">
+    <img src="image/logo.jpg" alt="" class="logo">
+    <div class="footer-ul-container">
+        <ul class="category">
+            <li class="category-title">men</li>
+            <li><a href="#" class="footer-link">t-shirt</a></li>
+            <li><a href="#" class="footer-link">sweatshirt</a></li>
+            <li><a href="#" class="footer-link">jeans</a></li>
+            <li><a href="#" class="footer-link">trousers</a></li>
+            <li><a href="#" class="footer-link">shoes</a></li>
+            <li><a href="#" class="footer-link">casuals</a></li>
+            <li><a href="#" class="footer-link">formals</a></li>
+            <li><a href="#" class="footer-link">sports</a></li>
+            <li><a href="#" class="footer-link">watch</a></li>
+        </ul>
+        <ul class="category">
+            <li class="category-title">women</li>
+            <li><a href="#" class="footer-link">t-shirt</a></li>
+            <li><a href="#" class="footer-link">sweatshirt</a></li>
+            <li><a href="#" class="footer-link">jeans</a></li>
+            <li><a href="#" class="footer-link">trousers</a></li>
+            <li><a href="#" class="footer-link">shoes</a></li>
+            <li><a href="#" class="footer-link">casuals</a></li>
+            <li><a href="#" class="footer-link">formals</a></li>
+            <li><a href="#" class="footer-link">sports</a></li>
+            <li><a href="#" class="footer-link">watch</a></li>
+        </ul>
+    </div>
+</div>
+<p class="footer-title">about company</p>
+<p class="info">trttte ttrytytrt5rt 5tyetrtryttyy tryuiuy  uuu6y6u65u yu65yu6u6u
+    jutyjutu  ukuykdyuk ukkyuk uuukjyukuk uy yujutyut yjty yjyuy yryryuutrutr ryutyutyuu
+    gkhyjghjku ytrtrrtrb ytyty yryytry tyhuuu77 5yte  rtrtt tyty hyukiut ukutyrtyhtr rth 
+    uyjjuyuu ytyuyuty yuyut yu7iuiyuoo ty7irteyeytye uyuyuiouoy ruyutytv hghgfh rttyu y 
+    rtytry yytyty t5yy65e5 errterty  y7u77 yuyu kyuhj     htggfrytrr tyu ttrttttttyyytt 
+    yuujyuui dsrfewrewr ertrt554 reerfd t5rtyyy ggggbgvgfrtr rgeeeeedd adasdrerr tytytyt 
+    rtghtghg gggbbvvnynj yjjmyht rferrrgfdfggthyt thyjyhjy tythytht rtrerertry hy7i8oi9o
+    8oi 6kuk ujyhhhh yuyyy gtgtyy   ikikioiuiutty  trtrrrtutyuty jutyuy6u juyuty ty uyutut
+</p>
+<p class="info">support emails - help@clothing.com,customersupport@clothing.com</p>
+<p class="info">telephone - 08037450222, 08146511747</p>
+<div class="footer-social-container">
+    <div>
+        <a href="#" class="social-link">terms & services</a>
+        <a href="#" class="social-link">privacy page</a>
+    </div>
+    <div>
+        <a href="#" class="social-link">instagram</a>
+        <a href="#" class="social-link">facebook</a>
+        <a href="#" class="social-link">twitter</a>
+        
+    </div>
+</div>
+<p class="footer-credit">Clothing, Best apparels online store</p>
+    ';
+}
+
+createFooter();
